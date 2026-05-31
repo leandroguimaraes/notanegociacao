e210eedcffb795d4e7a3d3cfd9b99aacf859c6ea
Leandro Martins Guimarães <leandropl@gmail.com>
2026-05-31 12:53:23 +0000

    Serialização e desserialização de notas

--- notanegociacao/corretora.py
+++ notanegociacao/corretora.py
@@ -1,7 +1,14 @@
 import os
+import json
+from pathlib import Path
+import datetime
 import pdfplumber
 
 from notanegociacao.notanegociacao import NotaNegociacao
+from notanegociacao.util import CustomEncoder, from_dict
+
+NOTAS_DIR = './notas'
+NOTAS_FILE = 'notas.json'
 
 
 class Corretora:
@@ -12,19 +19,64 @@ class Corretora:
             self.notasNegociacao = self.lerNotasDiretorio(dir)
 
     def lerNotasDiretorio(self, dir: str) -> list[NotaNegociacao]:
+        result = self.getNotasFromJson()
+
+        try:
+            notasPdf = sorted(
+                [f for f in os.listdir(dir) if f.endswith('.pdf')])
+
+            pdfCount = 1
+
+            for pdf in notasPdf:
+                # Extract date from filename (format: NotaNegociacao_48055_20250404.pdf)
+                pdf_date_str = pdf.split('_')[-1].replace('.pdf', '')
+                pdf_date = datetime.datetime.strptime(
+                    pdf_date_str, '%Y%m%d').date()
+
+                date_exists = any(nota.dataPregao ==
+                                  pdf_date for nota in result)
+
+                if date_exists:
+                    print(f'{pdf} já processada')
+                    pdfCount += 1
+                    continue
+
+                with pdfplumber.open(dir + pdf) as pdfFile:
+                    print(f'Processando PDFs: {pdfCount}/{len(notasPdf)}')
+                    print(pdf)
+                    pdfCount += 1
+
+                    for page in pdfFile.pages:
+                        result = NotaNegociacao.parseText(
+                            page.extract_text(), result)
+        finally:
+            result = sorted(result, key=lambda r: r.dataPregao)
+            self.saveNotasJson(result)
+
+        return result
+
+    def getNotasFromJson(self) -> list[NotaNegociacao]:
+        notas_file = self.getJsonPath()
+
         result: list[NotaNegociacao] = []
+        if notas_file.exists():
+            with open(notas_file, 'r') as f:
+                try:
+                    result = json.load(f)
+                    result = [from_dict(data, NotaNegociacao)
+                              for data in result]
+                except json.JSONDecodeError:
+                    result = []
 
-        notasPdf = sorted([f for f in os.listdir(dir) if f.endswith('.pdf')])
+        return result
 
-        pdfCount = 1
-        for pdf in notasPdf:
-            with pdfplumber.open(dir + pdf) as pdfFile:
-                print(f'Processando PDFs: {pdfCount}/{len(notasPdf)}')
-                print(pdf)
-                pdfCount += 1
+    def saveNotasJson(self, notas: list[NotaNegociacao]) -> None:
+        notas_file = self.getJsonPath()
+        with open(notas_file, 'w') as f:
+            json.dump(notas, f, cls=CustomEncoder, ensure_ascii=False)
 
-                for page in pdfFile.pages:
-                    result = NotaNegociacao.parseText(
-                        page.extract_text(), result)
+    def getJsonPath(self) -> Path:
+        notas_dir = Path(NOTAS_DIR)
+        notas_dir.mkdir(exist_ok=True)
 
-        return sorted(result, key=lambda r: r.dataPregao)
+        return notas_dir / NOTAS_FILE
--- notanegociacao/util.py
+++ notanegociacao/util.py
@@ -1,6 +1,49 @@
+import json
+import datetime
+from typing import Any, get_args, get_origin
+
+
 def strToFloat(value: str) -> float:
     return float(value.replace('.', '').replace(',', '.'))
 
 
 def strToInt(value: str) -> int:
     return int(value.replace('.', ''))
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
+
+        if hasattr(obj, '__dict__'):
+            return obj.__dict__
+
+        return super().default(obj)
+
+
+def from_dict[T](data: Any, cls: type[T]) -> T | datetime.date | list[Any | datetime.date | None] | None:
+    if data is None:
+        return None
+
+    if cls == datetime.date:
+        return datetime.date.fromisoformat(data)
+
+    if get_origin(cls) is list:
+        item_type = get_args(cls)[0]
+        return [from_dict(item, item_type) for item in data]
+
+    if isinstance(data, dict):
+        obj = cls.__new__(cls)
+
+        for field, field_type in cls.__annotations__.items():
+            if field in data:
+                setattr(
+                    obj,
+                    field,
+                    from_dict(data[field], field_type)
+                )
+
+        return obj
+
+    return data
