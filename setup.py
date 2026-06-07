440e677b53219858c68372e6e0bfa5512f03df8c
Leandro Martins Guimarães <leandropl@gmail.com>
2026-06-07 23:50:07 +0000

    Renomeando módulo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="notasnegociacao",
+    name="notanegociacao",
     version="1.0",
     description='Conversão de notas de corretagem em formato PDF para para uma estrutura de objetos Python',
     author='Leandro Martins Guimarães',
